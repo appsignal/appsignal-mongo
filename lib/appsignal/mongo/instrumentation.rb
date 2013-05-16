237e6f298630c42c233e66f64bb2bf4ae3f760ea
Robert Beekman <robert@matsimitsu.nl>
2013-05-16 17:53:50 +0200

    Deep clone the payload to prevent interference

--- lib/appsignal/mongo/instrumentation.rb
+++ lib/appsignal/mongo/instrumentation.rb
@@ -7,7 +7,7 @@ module Appsignal
 
       def instrument_with_appsignal_instrumentation(name, payload={}, &block)
         ActiveSupport::Notifications.instrument(
-          EVENT_NAME, name => payload) do
+          EVENT_NAME, name => Marshal.load(Marshal.dump(payload))) do
             instrument_without_appsignal_instrumentation(name, payload, &block)
           end
       end
