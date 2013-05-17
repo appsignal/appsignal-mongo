5284f1e61aa9697de0094c9a9903896464a0f580
Steven Weller <github@suweller.com>
2013-05-17 12:47:04 +0200

    Deep clone from the actual payload

--- lib/appsignal/mongo/instrumentation.rb
+++ lib/appsignal/mongo/instrumentation.rb
@@ -3,13 +3,30 @@ module Appsignal
     module Instrumentation
       EVENT_NAME = 'query.mongodb'.freeze
 
+      def self.deep_clone(value)
+        case value
+        when Hash
+          result = {}
+          value.each { |k, v| result[k] = deep_clone(v) }
+          result
+        when Array
+          value.map { |v| deep_clone(v) }
+        when Symbol, Numeric, true, false, nil
+          value
+        else
+          value.clone
+        end
+      end
+
       private
 
       def instrument_with_appsignal_instrumentation(name, payload={}, &block)
         ActiveSupport::Notifications.instrument(
-          EVENT_NAME, name => Marshal.load(Marshal.dump(payload))) do
-            instrument_without_appsignal_instrumentation(name, payload, &block)
-          end
+          EVENT_NAME,
+          name => Appsignal::Mongo::Instrumentation.deep_clone(payload)
+        ) do
+          instrument_without_appsignal_instrumentation(name, payload, &block)
+        end
       end
 
     end
